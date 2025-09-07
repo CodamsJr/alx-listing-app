5f0c4a07506a070d9013a49a6b9c9c8614a35a3d
CodamsJr <oguntolahammed076@gmail.com>
2025-09-07 14:53:55 +0100

    Create index.ts

---
+++ interfaces/index.ts
@@ -0,0 +1,13 @@
+// Reusable interfaces for props
+
+export interface CardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+export interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+}
